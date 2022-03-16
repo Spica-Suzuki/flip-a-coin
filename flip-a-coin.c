d4c588325a900326ca9e2225282c624818b27d68
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2022-03-17 01:25:48 +0300

    Update flip-a-coin.c

--- flip-a-coin.c
+++ flip-a-coin.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
  int main (){
- 	printf("Yazı-tura atmak için herhangi bir tuşa basınız \n\n");
+ 	printf("Press any key to flip a coin! \n\n");
  	int x;
  	scanf("%d",x);
  	srand(time(0));
  	short y = (rand() % 500) + 1;
- 		y >= 250 ? printf("yazı geldi\n") : printf("tura geldi\n");
+ 		y >= 250 ? printf("Heads!\n") : printf("Tails!\n");
  	return 0;}
