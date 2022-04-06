af2f3ec9beac431ed47a7bd8008948d78f0c1d9d
Supika <76227757+Spica-Suzuki@users.noreply.github.com>
2022-04-06 06:55:18 +0300

    Update flip-a-coin.c

--- flip-a-coin.c
+++ flip-a-coin.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
  int main (){
  	printf("Press any key to flip a coin! \n\n");
-  int x; scanf("%d",x);
+  short x; scanf("%d",x);
  	srand(time(0));
  	short y = (rand() % 500) + 1;
  		y >= 250 ? printf("Heads!\n") : printf("Tails!\n");
